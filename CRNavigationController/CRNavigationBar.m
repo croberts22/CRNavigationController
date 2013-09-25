118b49a101899465c1db0c1e51a36f6906f5b82e
Corey Roberts <croberts22@utexas.edu>
2013-09-24 21:56:23 -0500

    Fixing base library.

--- CRNavigationController/CRNavigationBar.m
+++ CRNavigationController/CRNavigationBar.m
@@ -19,31 +19,24 @@ static CGFloat const kSpaceToCoverStatusBars = 20.0f;
 
 - (void)setBarTintColor:(UIColor *)barTintColor {
     [super setBarTintColor:barTintColor];
+    
     if (self.colorLayer == nil) {
         self.colorLayer = [CALayer layer];
         self.colorLayer.opacity = kDefaultColorLayerOpacity;
         [self.layer addSublayer:self.colorLayer];
     }
+    
     self.colorLayer.backgroundColor = barTintColor.CGColor;
 }
 
 - (void)layoutSubviews {
     [super layoutSubviews];
+    
     if (self.colorLayer != nil) {
         self.colorLayer.frame = CGRectMake(0, 0 - kSpaceToCoverStatusBars, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds) + kSpaceToCoverStatusBars);
         
         [self.layer insertSublayer:self.colorLayer atIndex:1];
     }
-}
-
-- (void)displayColorLayer:(BOOL)display animated:(BOOL)animated {
-    float duration = animated ? 0.3f : 0.0f;
-    float alpha = display ? 0.3f : 0.0f;
-    
-    [UIView animateWithDuration:duration
-                     animations:^{
-                     
-                     }];
 }
 
 @end
